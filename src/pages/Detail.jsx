325f267f1e179adfded7b3451c10bf647475d9c9
SUN-AAA <seouna06@gmail.com>
2025-09-06 22:29:41 +0900

    add: Routing

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,10 @@
+import {Link} from "react-router-dom"
+
 const Detail = () => {
     return(
         <div>
             <h1>Detail</h1>
+            <Link to={"/"}>Home</Link>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import {Link} from "react-router-dom"
+
 const Home = () => {
     return(
         <div>
             <h1>AiWiki</h1>
+            <Link to={"/detail"}>Detail</Link>
         </div>
     )
 }
