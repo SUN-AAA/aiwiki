e19c5c91542c358e215277708c7d6e54fe6c3084
SUN-AAA <seouna06@gmail.com>
2025-09-07 19:17:11 +0900

    add: Deatil.css

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom"
 import { useLocation } from "react-router-dom";
+import "../styles/Detail.css"
 
 function Detail() {
   const location = useLocation();
   const { tool } = location.state;
 
   return (
-    <div>
+    <div className="Detail">
       <h1>{tool.name}</h1>
       <p>{tool.content}</p>
       <a href={tool.link} target="_blank" rel="noopener noreferrer">Go to Homepage</a>
-      <Link to={"/"}>Home</Link>
+      <div className="link">
+        <Link to={"/"}>Home</Link>
+      </div>
     </div>
   );
 }
