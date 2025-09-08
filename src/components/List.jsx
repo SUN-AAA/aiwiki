8b541c5fd616a535c994c4cca6601dfb37e7521a
SUN-AAA <seouna06@gmail.com>
2025-09-08 22:51:17 +0900

    add: data/Tools.jsx, fix: List.jsx tools data

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,34 +2,9 @@ import "../styles/List.css"
 import {Link} from "react-router-dom"
 import { useState } from "react"
 import getFilteredData from "../utils/getFilteredData"
+import Tools from "../data/Tools"
 
 const List = () => {
-    const tools = [
-        {
-            name: "Chat Gpt",
-            price: "부분 유료",
-            category: "대화형 인공지능",
-            platform: "웹/앱",
-            content: "Chat Gpt : OpenAI 플랫폼 회원가입 후 무료 혹은 유료 구독제로 이용할 수 있으며, 회원가입을 하고 채팅을 하듯이 챗봇에 질문을 입력하는 것만으로 AI를 이용할 수 있다. 어떤 유형의 글을 입력해도 이에 맞는 답변이 생성되는 시도가 이루어진다. 스마트폰으로도 OpenAI 플랫폼에 들어가면 이용할 수 있다.",
-            link: "https://chatgpt.com/"
-        },
-        {
-            name: "Figma",
-            price: "부분 유료",
-            category: "디자인, 협업",
-            platform: "웹",
-            content: "Figma : 웹 기반 UI/UX 디자인 및 프로토타이핑, 협업툴 플랫폼이다. 부분 유료 프로그램이지만, 개인으로 사용한다면 사실상 무료 프로그램이다. 피그마는 자체가 웹 기반이기 때문에 피그마 계정 소유자가 링크를 공유하여 여러 명이 아트 보드를 확인하며 동시에 온라인으로 실시간 작업을 진행할 수 있다. 디자이너와 개발자의 소통을 도와주는 기능이 많다. 개발자가 참조할 수 있는 정보를 주는 ‘개발 툴바’가 있으며, 마우스 툴바로도 수칫값을 확인할 수 있어 가이드라인이 없어도 빠른 작업이 가능하다.",
-            link: "https://www.figma.com/ko-kr/ai/"
-        },
-        {
-            name: "Cursor",
-            price: "부분 유료",
-            category: "코딩",
-            platform: "통합개발환경(IDE)",
-            content: "Cursor : Visual Studio Code를 포크해서 만들어진 통합 개발 환경으로, 코드 작성, 디버깅, 수정 등 다양한 개발 작업을 AI의 도움으로 효율화할 수 있는 툴이다. Cursor는 GPT-4o, Claude 4 Sonnet 등 다양한 AI 모델을 지원하며, 코드 작성 및 디버깅 속도를 대폭 향상시키는 것을 목표로 하고 있다. 또한 자연어를 활용해 코드 작성, 수정, 명령어 변환 등 다양한 작업을 간단히 수행할 수 있다.",
-            link: "https://cursor.com/"
-        },
-    ]
 
     const [search, setSearch] = useState("")
     
@@ -37,7 +12,7 @@ const List = () => {
         setSearch(e.target.value)
     }
 
-    const filteredtool = getFilteredData({search, tools})
+    const filteredtool = getFilteredData({search, tools: Tools})
 
     return (
         <div className="List">
